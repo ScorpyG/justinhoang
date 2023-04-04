fbfaffe2c5accfa12d2f64d5ead99e5b208a7cef
scorpy <justinhoang90@gmail.com>
2023-04-03 21:03:12 -0700

    feat: slider animation for about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -3,11 +3,67 @@ import Head from 'next/head';
 import Link from 'next/link';
 import { useState } from 'react';
 import styles from './about.module.scss';
+import techList from '@/utilities/constants/techList';
 
 export default function About() {
   const [isProfileUrl, setProfileUrl] = useState(false);
   const headshotImgUrl = './images/profile.jpg';
   const ghostImgUrl = './images/ghost.gif';
+
+  const biography = (
+    <div className={`${styles.header}`}>
+      <img
+        src={isProfileUrl ? headshotImgUrl : ghostImgUrl}
+        width={260}
+        alt="Headshot"
+        onClick={() => setProfileUrl((profileUrl) => !profileUrl)}
+      />
+  
+      <div className={`${styles.bio}`}>
+        <h1>ABOUT ME</h1>
+        <p>
+          I'm a software developer based in <u>Vancouver</u> with a passion for
+          technology and design. I enjoy learning different technologies and
+          building applications to challenge my own creativity, problem-solving,
+          planning, and organization.
+        </p>
+        <p>
+          Skills/Interests:{' '}
+          <b>
+            TypeScript, JavaScript, React.js, Git, NoSQL, Python, Next.js, Saas,
+            Machine Learning, A.I, Robotics
+          </b>
+        </p>
+        <br />
+  
+        <Link href={resume}>resume.docx</Link>
+      </div>
+    </div>
+  );
+
+  const technologies = (
+    <div className={`${styles.technical}`}>
+      <div className={`${styles.carousel}`}>
+        {techList.map((technology, key) => 
+          <div className={`${styles.technology}`} key={key}>
+            {technology.icon}
+            <h3>{technology.name}</h3>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+
+  const hobbies = (
+    <div className={`${styles.hobbies}`}>
+      <h1>Hobbies</h1>
+      <p>A bit of...insights!</p>
+      
+      <div className={`${styles.cards}`}>
+
+      </div>
+    </div>
+  );
 
   return (
     <>
@@ -16,48 +72,9 @@ export default function About() {
       </Head>
       <main>
         <div className={`${styles.container}`}>
-          <div className={`${styles.header}`}>
-            <img
-              src={isProfileUrl ? headshotImgUrl : ghostImgUrl}
-              width={260}
-              alt="Headshot"
-              onClick={() => setProfileUrl((profileUrl) => !profileUrl)}
-            />
-
-            <div className={`${styles.bio}`}>
-              <h1>ABOUT ME</h1>
-              <p>
-                I'm a software developer based in <u>Vancouver</u> with a passion for
-                technology and design. I enjoy learning different technologies and
-                building applications to challenge my own creativity, problem-solving,
-                planning, and organization.
-              </p>
-              <p>
-                Skills/Interests:{' '}
-                <b>
-                  TypeScript, JavaScript, React.js, Git, NoSQL, Python, Next.js, Saas,
-                  Machine Learning, A.I, Robotics
-                </b>
-              </p>
-              <br />
-
-              <Link href={resume}>resume.docx</Link>
-            </div>
-          </div>
-          <div className={`${styles.technical}`}>
-            <div className={`${styles.technology}`}>
-                <i></i>
-                <h3>TypeScript</h3>
-            </div>
-          </div>
-          <div className={`${styles.hobbies}`}>
-            <h1>Hobbies</h1>
-            <p>A bit of...insights!</p>
-            
-            <div className={`${styles.cards}`}>
-
-            </div>
-          </div>
+          {biography}
+          {technologies}
+          {hobbies}
         </div>
       </main>
     </>
--- src/utilities/constants/techList.tsx
+++ src/utilities/constants/techList.tsx
@@ -1,51 +1,70 @@
+import JavaScript from "../svg/JavaScript";
+import TypeScript from "../svg/TypeScript";
+import React from "../svg/React";
+import HTML from "../svg/HTML";
+import CSS from "../svg/CSS";
+import SASS from "../svg/SASS";
+import Python from "../svg/Python";
+import Database from "../svg/Database";
+import Vue from "../svg/Vue";
+import Code from "../svg/Code";
+import Node from "../svg/Node";
+import Next from "../svg/Next";
 
+export type TechnologyLogo = {
+  name: string;
+  icon: JSX.Element;
+}
 
 const techList = [
-    {
-      name: 'TypeScript',
-      icon: ''
-    },
-    {
-      name: 'JavaScript',
-      icon: '',
-    },
-    {
-      name: 'React.js',
-      icon: '',
-    },
-    {
-      name: 'Next.js',
-      icon: '',
-    },
-    {
-      name: 'HTML',
-      icon: '',
-    },
-    {
-      name: 'CSS',
-      icon: '',
-    },
-    {
-      name: 'SASS',
-      icon: '',
-    },
-    {
-      name: 'Python',
-      icon: '',
-    },
-    {
-      name: 'MongoDB',
-      icon: '',
-    },
-    {
-      name: 'Vue.js',
-      icon: '',
-    },
-    {
-      name: 'C++',
-      icon: '',
-    },
+  {
+    name: 'TypeScript',
+    icon: <TypeScript/> 
+  },
+  {
+    name: "JavaScript",
+    icon: <JavaScript/>,
+  },
+  {
+    name: 'React.js',
+    icon: <React/>
+  },
+  {
+    name: 'Next.js',
+    icon: <Next/>
+  },
+  {
+    name: 'HTML',
+    icon: <HTML/>
+  },
+  {
+    name: 'CSS',
+    icon: <CSS/>
+  },
+  {
+    name: 'SASS',
+    icon: <SASS/>
+  },
+  {
+    name: 'Python',
+    icon: <Python/>
+  },
+  {
+    name: 'MongoDB',
+    icon: <Database/>
+  },
+  {
+    name: 'Node.js',
+    icon: <Node/>
+  },
+  {
+    name: 'Vue.js',
+    icon: <Vue/>
+  },
+  {
+    name: 'C++',
+    icon: <Code/>
+  },
 ];
 
-
-  export default techList;
+export default techList;
