182feaf01f0705e6a6a02d217752ebf7f6190823
scorpy <justinhoang90@gmail.com>
2023-04-07 19:35:00 -0700

    feat: complete projects page at minimum

--- src/components/Card/ProjectCard.tsx
+++ src/components/Card/ProjectCard.tsx
@@ -1,4 +1,5 @@
 import { ProjectCardData } from '@/utilities/type/types';
+import Link from 'next/link';
 import styles from './card.module.scss';
 
 export interface ProjectCardProps {
@@ -9,5 +10,20 @@ export interface ProjectCardProps {
 export default function ProjectCard(props: ProjectCardProps) {
   const { project } = props;
 
-  return <div className={`${styles.projectCard}`}></div>;
+  return (
+    <div className={`${styles.projectCard}`}>
+      <Link href={project.siteLink} target="_blank">
+        <img src={project.image} alt="Project GIF" />
+      </Link>
+
+      <div className={`${styles.projectInfo}`}>
+        <Link href={project.repoLink} target="_blank">
+          <h2>{project.title}</h2>
+        </Link>
+
+        <h3>{project.technologies.join(' | ').toString()}</h3>
+        <p>{project.desc}</p>
+      </div>
+    </div>
+  );
 }
