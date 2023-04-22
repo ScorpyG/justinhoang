1c2d40d80d12abf6bcb8ce4e547aa13712525811
scorpy <justinhoang90@gmail.com>
2023-04-22 16:36:46 -0700

    style: adding color mode

--- src/components/TopNavbar/TopNavbar.tsx
+++ src/components/TopNavbar/TopNavbar.tsx
@@ -1,14 +1,26 @@
 import { routeUrls } from '@/utilities/constants/URLs';
-import { Box, Flex, IconButton, Menu, MenuButton, MenuItem, MenuList, Spacer } from '@chakra-ui/react';
+import LightLogo from '@/utilities/svgr/LightLogo';
+import { CloseIcon, HamburgerIcon, MoonIcon, SunIcon } from '@chakra-ui/icons';
+import {
+  Box,
+  Flex,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Spacer,
+  useColorMode,
+  useColorModeValue,
+} from '@chakra-ui/react';
 import Link from 'next/link';
 import { useState } from 'react';
-import CloseIcon from '../../utilities/svgr/Close';
 import DarkLogo from '../../utilities/svgr/DarkLogo';
-import Hamburger from '../../utilities/svgr/Hamburger';
 import styles from './topnavbar.module.scss';
 
 export default function TopNavbar() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const { toggleColorMode } = useColorMode();
 
   return (
     <Flex
@@ -20,22 +32,30 @@ export default function TopNavbar() {
       margin={'auto'}
     >
       <Box className={styles.logo}>
-        <Link href="/">
-          <DarkLogo />
-        </Link>
+        <Link href="/">{useColorModeValue(<DarkLogo />, <LightLogo />)}</Link>
       </Box>
 
       <Spacer />
+      <IconButton
+        aria-label="Theme Toggle"
+        icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
+        onClick={toggleColorMode}
+        variant={'outline'}
+        border={'2px'}
+        borderRadius={'9px'}
+        h={'45px'}
+        w={'45px'}
+        mr={'10px'}
+      />
 
       <Menu closeOnSelect autoSelect={false}>
         <MenuButton
           onClick={() => setMenuOpen(!menuOpen)}
-          aria-label="Options"
+          aria-label="Menu Options"
           variant={'outline'}
           as={IconButton}
-          icon={menuOpen ? <CloseIcon /> : <Hamburger />}
+          icon={menuOpen ? <CloseIcon /> : <HamburgerIcon />}
           border={'2px'}
-          borderColor={'black'}
           borderRadius={'9px'}
           h={'45px'}
           w={'45px'}
