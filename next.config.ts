485cc18016f3fc0831f93d285ee25f8ed4478be6
scorpy <justinhoang90@gmail.com>
2025-05-15 22:23:56 -0700

    ci: setup linting and commitlint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: { ignoreDuringBuilds: false },
+  compiler: {
+    removeConsole:
+      process.env.NODE_ENV === 'production' ? { exclude: ['error'] } : false,
+  },
 };
 
 export default nextConfig;
